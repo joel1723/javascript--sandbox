b6045b4e331bf13527512b66bd69828e99b100ea
joel1723 <75437050+joel1723@users.noreply.github.com>
2021-06-27 19:23:09 -0400

    new commit

---
+++ data types/app.js
@@ -0,0 +1,42 @@
+// 1 ..PRIMITIVE types
+
+
+
+//strings
+const name = 'joel bum '
+
+
+//number
+const age = 31;
+
+// boolean 
+// booleans do not hav quotes
+const hasKids = false 
+
+// null
+const car = null
+
+// undefined
+let test;
+
+// symbol
+
+const sym =  Symbol();
+
+
+//2 .. REFRENCE TYPES- all objects
+
+
+// array
+const hobbies= ['movies','basketball','video ','music']
+//object  literal
+const  address = {
+    city: 'orlando',
+    state: 'florida',
+
+}
+
+//date - D in Date must capital 
+const today= new Date()
+console.log(today)
+
